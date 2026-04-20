adc86a8b49013a72f9354f8acf8d953bc6e0eda2
Thaizan666666 <Thaizan246810@gmail.com>
2026-04-20 18:25:19 +0700

    Create_Manager_Scene

---
+++ Assets/[00]Script/Manager/ManagerScene.cs
@@ -0,0 +1,37 @@
+﻿using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ManagerScene : MonoBehaviour
+{
+    public static ManagerScene Instance;
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    public void LoadSceneMomHappyEnding()
+    {
+        //SceneManager.LoadScene();                //ได้ไอตืม
+    }
+    public void LoadSceneMomSadEnding()
+    {
+        //SceneManager.LoadScene();                //อดแดก
+    }
+    public void LoadSceneBadEnding()
+    {
+        //SceneManager.LoadScene();                //มเร็งแดก
+    }
+    public void MainMenu()
+    {
+        //SceneManager.LoadScene();                //หน้าMenu
+    }
+}
